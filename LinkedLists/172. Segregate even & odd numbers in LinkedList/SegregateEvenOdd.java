5215e6889859a3b1ec88d2b45499c65feb9f0d90
Raj Karmakar <97rajmath@gmail.com>
2024-03-11 11:34:39 +0530

    Added Segregate Even Odd in LinkedList Problem

--- LinkedLists/172. Segregate even & odd numbers in LinkedList/SegregateEvenOdd.java
+++ LinkedLists/172. Segregate even & odd numbers in LinkedList/SegregateEvenOdd.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Node {
 	int data;
 	Node next;
@@ -30,7 +32,51 @@ class LinkedList {
 			System.out.print(currentNode.data + "->");
 			currentNode = currentNode.next;
 		}
-		System.out.println();
+		System.out.print("null\n");
+	}
+
+	int getNodeCount() {
+		int count = 0;
+		Node current = head;
+
+		while (current != null) {
+			count++;
+			current = current.next;
+		}
+		return count;
+	}
+
+	Node segregateEvenOdd() {
+		int[] data = new int[this.getNodeCount()];
+		Node current = head;
+		int index = 0;
+
+		while (current != null) {
+			data[index++] = current.data;
+			current = current.next;
+		}
+
+		int j = 0;
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] % 2 == 0) {
+				if (i != j) {
+					int temp = data[i];
+					data[i] = data[j];
+					data[j] = temp;
+				}
+				j++;
+			}
+		}
+
+		System.out.println("data" + Arrays.toString(data));
+
+		current = head;
+		index = 0;
+		while (current != null) {
+			current.data = data[index++];
+			current = current.next;
+		}
+		return head;
 	}
 }
 
@@ -38,6 +84,17 @@ public class SegregateEvenOdd {
 	public static void main(String[] args) {
 		LinkedList linkedList = new LinkedList();
 		linkedList.insert(17);
+		linkedList.insert(8);
+		linkedList.insert(2);
+		linkedList.insert(9);
+		linkedList.insert(11);
+		linkedList.insert(13);
+		linkedList.insert(20);
+		linkedList.insert(22);
 
+		linkedList.display();
+
+		linkedList.segregateEvenOdd();
+		linkedList.display();
 	}
 }
