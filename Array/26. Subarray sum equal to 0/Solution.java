96d72b6c28d35974748d46257d3020daecfd67f3
Raj Karmakar <97rajmath@gmail.com>
2024-05-08 12:50:03 +0530

    SubArray with sum equal to 0

---
+++ Array/26. Subarray sum equal to 0/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+	static boolean findSum(int arr[], int n) { // GFG Accepted Solution
+		Set<Integer> set = new HashSet();
+		int currentSum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			currentSum += arr[i];
+
+			if (currentSum == 0 || arr[i] == 0 || set.contains(currentSum)) {
+				return true;
+			}
+
+			set.add(currentSum);
+		}
+
+		return false;
+	}
+}
