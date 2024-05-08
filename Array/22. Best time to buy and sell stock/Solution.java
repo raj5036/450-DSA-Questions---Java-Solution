e9dd5eae93848fdd6a2e13b435cc6abac2859173
Raj Karmakar <97rajmath@gmail.com>
2024-05-08 12:36:06 +0530

    Added new Questions

---
+++ Array/22. Best time to buy and sell stock/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+	public int maxProfit_TLE(int[] prices) { // LeetCode: Time Limit Exceeded
+		int maxProfit = 0;
+		for (int i = 0; i < prices.length - 1; i++) {
+			for (int j = i + 1; j < prices.length; j++) {
+				int buyingPrice = prices[i], sellingPrice = prices[j];
+				if (buyingPrice > sellingPrice) {
+					continue;
+				}
+				maxProfit = Integer.max(maxProfit, sellingPrice - buyingPrice);
+			}
+		}
+		return maxProfit;
+	}
+
+	public int maxProfit(int[] prices) {
+		int buyPrice = prices[0];
+		int profit = 0;
+
+		for (int i = 1; i < prices.length; i++) {
+			if (buyPrice > prices[i]) {
+				buyPrice = prices[i];
+			}
+
+			profit = Math.max(profit, prices[i] - buyPrice);
+		}
+
+		return profit;
+	}
+}
