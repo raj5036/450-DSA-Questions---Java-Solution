0893bcd1aae6719c2a05f4a191eb2f3dab6085a9
Raj Karmakar <97rajmath@gmail.com>
2024-05-10 12:04:01 +0530

    Updates

--- Test.java
+++ Test.java
@@ -1,24 +1,49 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+	int data;
+	TreeNode left, right;
+
+	TreeNode(int data) {
+		this.data = data;
+		this.left = this.right = null;
+	}
+}
+
+class BinaryTree {
+	TreeNode root;
+
+	BinaryTree() {
+		this.root = null;
+	}
+
+	BinaryTree(int data) {
+		this.root = new TreeNode(data);
+	}
+
+	void leftView() {
+		List<TreeNode> list = new ArrayList<>();
+		leftViewUtil(root, list, 0);
+	}
+
+	void leftViewUtil(TreeNode node, List<TreeNode> list, int level) {
+		if (node == null) {
+			return;
+		}
+
+		if (level == list.size()) {
+			System.out.print(node.data + " ");
+			list.add(node);
+		}
+
+		leftViewUtil(node.left, list, level + 1);
+		leftViewUtil(node.right, list, level + 1);
+	}
+}
 
 public class Test {
 	public static void main(String[] args) {
-		Map<Integer, String> map = new HashMap<>();
-		map.put(-1, "-1");
-		map.put(-3, "-3");
-		map.put(-2, "-2");
-		map.put(3, "3");
-		map.put(-25, "-25");
 
-		map.forEach((k, v) -> {
-			System.out.println(k + "->" + v);
-		});
-		System.out.println();
-		SortedSet<Integer> sortedKeys = new TreeSet<>(map.keySet());
-		for (Integer key : sortedKeys) {
-			System.out.println(map.get(key));
-		}
 	}
 }
