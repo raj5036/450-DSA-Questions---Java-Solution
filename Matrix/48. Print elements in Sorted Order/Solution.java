0eea2577f1716409ce85937d9f99ecf53cee698b
Raj Karmakar <97rajmath@gmail.com>
2024-05-10 12:43:23 +0530

    New Questions added

---
+++ Matrix/48. Print elements in Sorted Order/Solution.java
@@ -0,0 +1,7 @@
+// Use Priority Queue
+
+public class Solution {
+	public static void main(String[] args) {
+
+	}
+}
--- Test.java
+++ Test.java
@@ -1,49 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class TreeNode {
-	int data;
-	TreeNode left, right;
-
-	TreeNode(int data) {
-		this.data = data;
-		this.left = this.right = null;
-	}
-}
-
-class BinaryTree {
-	TreeNode root;
-
-	BinaryTree() {
-		this.root = null;
-	}
-
-	BinaryTree(int data) {
-		this.root = new TreeNode(data);
-	}
-
-	void leftView() {
-		List<TreeNode> list = new ArrayList<>();
-		leftViewUtil(root, list, 0);
-	}
-
-	void leftViewUtil(TreeNode node, List<TreeNode> list, int level) {
-		if (node == null) {
-			return;
-		}
-
-		if (level == list.size()) {
-			System.out.print(node.data + " ");
-			list.add(node);
-		}
-
-		leftViewUtil(node.left, list, level + 1);
-		leftViewUtil(node.right, list, level + 1);
-	}
-}
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Test {
 	public static void main(String[] args) {
+		Map<Integer, String> Umap = new HashMap<>();
+		Map<Integer, String> Omap = new TreeMap<>();
 
+		int[] keys = { -3, -2, -1, 0, 1, 2, 3 };
+		String[] values = { "-3", "-2", "-1", "0", "1", "2", "3" };
+
+		for (int i = 0; i < keys.length; i++) {
+			Umap.put(keys[i], values[i]);
+			Omap.put(keys[i], values[i]);
+		}
+
+		System.out.println("Un-ordered Map");
+		Umap.forEach((k, v) -> {
+			System.out.print(v + " ");
+		});
+		System.out.println("\nOrdered Map");
+		Omap.forEach((k, v) -> {
+			System.out.print(v + " ");
+		});
 	}
 }
