2a17e67f567fcca9135e7df0ea3c2ce54203aed0
Raj Karmakar <97rajmath@gmail.com>
2024-05-13 13:01:52 +0530

    Print Anagrams together

--- String/91. Print all Anagrams together/Solution.java
+++ String/91. Print all Anagrams together/Solution.java
@@ -1,3 +1,34 @@
-public class Solution {
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+	public List<List<String>> Anagrams(String[] words) {
+		// Code here
+		List<List<String>> answers = new ArrayList<>();
+		Map<String, List<String>> map = new HashMap<>();
+
+		for (String word : words) {
+			int[] key = new int[26];
+			Arrays.fill(key, 0);
+
+			for (int i = 0; i < word.length(); i++) {
+				char c = word.charAt(i);
+				int index = c - 'a';
+				key[index]++;
+			}
+
+			// System.out.println(Arrays.toString(key));
+			String wordKey = Arrays.toString(key);
+
+			if (!map.containsKey(wordKey)) {
+				map.put(wordKey, new ArrayList<>());
+			}
+			map.get(wordKey).add(word);
+		}
+		map.forEach((k, v) -> {
+			List<String> answer = v;
+			answers.add(answer);
+		});
+		return answers;
+	}
 }
